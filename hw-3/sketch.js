f7a5003ebe26fb437622bfe41604379cf97c8df9
Christine Martin <christine2.martin@uconnect.umt.edu>
2019-01-24 01:16:41 -0700

    add canvas, background and Hello WOrld

---
+++ hw-3/sketch.js
@@ -0,0 +1,17 @@
+//Declare setup function
+function setup(){
+
+//create a canvas 600 px wide and 400 px high
+    createCanvas( 600,400);
+
+//color the background 'pink'
+    background( 'pink');
+}
+
+//Declare the draw function
+function draw() {
+
+  //tell p5 to print the text string "Hello WOrld!"
+  //this is going to be positioned at ( x: 20, y: 30).
+  text( "Hello World!",20,30);
+}
